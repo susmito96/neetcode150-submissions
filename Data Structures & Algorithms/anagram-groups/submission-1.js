860181c657e84bb7640d7a360220acc518bbce8f
susmito96 <susmito.learn@gmail.com>
2026-09-14 14:46:46 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const strMap = {};
+
+        for (let str of strs) {
+            let sortedStr = str.split("").sort().join();
+            if (strMap.hasOwnProperty(sortedStr)) {
+                strMap[sortedStr].push(str);
+            } else {
+                strMap[sortedStr] = [str];
+            }
+        }
+
+        return Object.keys(strMap).reduce(
+            (finalArr, curr) => {
+                finalArr.push(strMap[curr]);
+                return finalArr;
+            },
+            []
+        )
+    }
+}
